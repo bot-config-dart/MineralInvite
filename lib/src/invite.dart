7f704aef9a01b449168205c00c6132da903ef58a
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-03-30 17:11:32 +0200

    bcp de trucs !

--- lib/src/invite.dart
+++ lib/src/invite.dart
@@ -1,17 +1,16 @@
 import 'package:mineral/core/extras.dart';
-import 'package:mineral/framework.dart';
+import 'package:mineral_contract/mineral_contract.dart';
 import 'package:mineral_invite/src/events/guild_create.dart';
 
 import 'entities/guild_invite_cache.dart';
 import 'events/member_add.dart';
 
-class MineralInvitationsModule extends MineralModule with Container {
-  MineralInvitationsModule (): super('InvitationsMineralSnakeCase', 'InvitationsMineral', "Le module d'invitation");
+class MineralInvitationsModule extends MineralPackageContract with Container {
+  MineralInvitationsModule (): super('InvitationsMineral', "Le module d'invitation");
+  final GuildInviteCache inviteCache = GuildInviteCache();
 
   @override
   Future<void> init () async {
-    container.bind((_) => GuildInviteCache());
-
     commands.register([]);
     events.register([
       GuildCreate(),
--- lib/src/mixins/invite.dart
+++ lib/src/mixins/invite.dart
@@ -1,6 +1,7 @@
+import 'package:mineral_invite/mineral_invite.dart';
 import 'package:mineral_invite/src/entities/guild_invite_cache.dart';
 import 'package:mineral_ioc/ioc.dart';
 
 mixin InvitesMineral {
-  GuildInviteCache get invitesCache => ioc.use<GuildInviteCache>();
+  GuildInviteCache get invitesCache => ioc.use<MineralInvitationsModule>().inviteCache;
 }
