5011cce2991c0c04a2447c090d552dec9a1e0af7
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-07-17 23:32:42 +0200

    refactor: spaces

--- lib/src/events/member_add.dart
+++ lib/src/events/member_add.dart
@@ -12,20 +12,18 @@ class MineralInviteMemberJoin extends MineralEvent<MemberJoinEvent> with Invites
   @override
   Future<void> handle(event) async {
     final cachedInvites = invitesCache.invites.getOrFail(event.member.guild.id);
-    final invites = event.member.guild.invites.cache;
+    final invites = await event.member.guild.invites.sync();
     Invite? invite = invites.find((element) => !cachedInvites.invites.containsKey(element) && cachedInvites.invites.get(element.code)!.uses < element.uses);
     EventService manager = container.use<EventService>();
 
     if (invite == null) {
       if(event.guild.features.contains(GuildFeature.vanityUrl) && event.guild.vanity!.uses > invitesCache.vanityInvite!.uses) {
-        manager.controller.add(InviteMemberAdd(event, null, vanityInvite:  event.guild.vanity,
-            isVanity: true));
+        manager.controller.add(InviteMemberAdd(event, null, vanityInvite: event.guild.vanity, isVanity: true));
         return;
       }
     }
 
     await cachedInvites.sync();
-    manager.controller.add(InviteMemberAdd(event, invite,
-        isVanity: false));
+    manager.controller.add(InviteMemberAdd(event, invite, isVanity: false));
   }
 }
